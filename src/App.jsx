c62ed80be0aac6273b996f971ae3346a05210bc9
Vedant-VB07 <vedantbondekar@gmail.com>
2025-12-26 11:21:45 +0530

    bg changed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <BrowserRouter>
+    <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <Navbar />
       <Routes>
         {/* Todo page stays HOME */}
